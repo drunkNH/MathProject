73ede05dde7bcb7c791f6f24f8b064f8bca2d472
alexanderamores <83688596+alexanderamores@users.noreply.github.com>
2021-05-06 21:57:40 -0700

    Add files via upload

--- MathGameProject/src/MathGame.java
+++ MathGameProject/src/MathGame.java
@@ -1,44 +1,45 @@
+
 public class MathGame 
 {
-    public void startGame()
-    {
-        //possible start screen
-        //System.out.println("MATH GAME");
-
-        for(;;)
-        {
-            System.out.println("ENTER YOUR NAME.");
-
-
-            //GAME STARTING, 10 QUESTIONS
-            for(int i=0; i>10; i++)
-            {
-                int score = 0;
-                int number1 = 0; //random number 1-100
-                int number2 = 0; //random number 1-100
-                int questionType = 0; //random number 1-4, 1:+ 2:- 3:* 4:/
-                boolean correct = false;
-                //create a question function passes three variables
-
-                //gets the new 3 randomized numbers
-
-
-                //calls the answer function which returns true or false
-                //correct = function()
-
-                if(correct == true)
-                    score += 1;
-            }//end of one cycle
-
-            //calls function that takes in score and name
-
-
-            //asks do you want to play again screen
-            System.out.println("DO YOU WANT TO PLAY AGAIN Y/N");
-            // scanner = 
-
-            //if(scanner == "no")
-                break;
-        }
-    }//end of startGame()
+	public void startGame()
+	{
+		//possible start screen
+		//System.out.println("MATH GAME");
+		
+		for(;;)
+		{
+			System.out.println("ENTER YOUR NAME.");
+		
+		
+			//GAME STARTING, 10 QUESTIONS
+			for(int i=0; i>10; i++)
+			{
+				int score = 0;
+				int number1 = 0; //random number 1-100
+				int number2 = 0; //random number 1-100
+				int questionType = 0; //random number 1-4, 1:+ 2:- 3:* 4:/
+				boolean correct = false;
+				//create a question function passes three variables
+			
+				//gets the new 3 randomized numbers
+			
+			
+				//calls the answer function which returns true or false
+				//correct = function()
+			
+				if(correct == true)
+					score += 1;
+			}//end of one cycle
+		
+			//calls function that takes in score and name
+		
+		
+			//asks do you want to play again screen
+			System.out.println("DO YOU WANT TO PLAY AGAIN Y/N");
+			// scanner = 
+			
+			//if(scanner == "no")
+				break;
+		}
+	}//end of startGame()
 }
