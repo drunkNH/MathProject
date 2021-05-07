396a42c1fc0aed573f2e5d41bb9b08a4c9cc48b4
vikram-elango <83794714+vikram-elango@users.noreply.github.com>
2021-05-06 20:58:49 -0700

    Add files via upload

---
+++ MathGameProject/src/Timer1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Timer1 {
+static int interval;
+static Timer timer;
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int secs=10;
+    int delay = 1000;
+    int period = 1000;
+    timer = new Timer();
+    interval = (secs);
+    System.out.println(secs);
+    timer.scheduleAtFixedRate(new TimerTask() {
+
+        public void run() {
+            System.out.println(setInterval());
+
+        }
+    }, delay, period);
+}
+
+private static final int setInterval() {
+    if (interval == 1)
+        timer.cancel();
+    return --interval;
+}
+}
