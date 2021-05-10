0a2feb453e9b8c0fa8776bc2689281d802cf3f81
Jeffrey <Jeffrey@DESKTOP-HHNATK4>
2021-05-10 14:44:32 -0700

    Hiscore capabilities

--- MathGameProject/src/Hiscore.java
+++ MathGameProject/src/Hiscore.java
@@ -1,14 +1,13 @@
 import java.util.*;
 import java.util.Map.Entry;
-import javax.swing.*;
-import java.awt.event.*;
-public class Hiscore 
-{
+import java.util.Comparator;
+
+public class Hiscore {
+	
 	//method to store a player's high score with their name.
 	//if a name is the same but score is higher, it updates the score, if score is lower, just returns the map with no changes
-	public static TreeMap<String, Integer> calculateScores (String name, int score, TreeMap<String, Integer> hiscores)
+	public static TreeMap<String, Integer> storeScore (String name, int score, TreeMap<String, Integer> hiscores)
 	{
-		
 		for(Entry<String, Integer> entry: hiscores.entrySet())
 		{
 			if (entry.getKey().equals(name))
@@ -18,82 +17,57 @@ public class Hiscore
 		hiscores.put(name, score);
 		return hiscores;
 	}
-		
+	
 	//method to sort by values instead of key
-	public static <K, V extends Comparable<V>> Map<K, V> sortByValues(final Map<K, V> map) 
-	{
-		Comparator<K> valueComparator =  new Comparator<K>() 
-		{
-			public int compare(K k1, K k2) 
-			{
-				int compare = map.get(k2).compareTo(map.get(k1));
-				if (compare == 0) 
-					return 1;
-				else 
-					return compare;
-			}
-		};
-		    Map<K, V> sortedByValues = new TreeMap<K, V>(valueComparator);
-		    sortedByValues.putAll(map);
-		    return sortedByValues;
+	public static <K, V extends Comparable<V>> Map<K, V> sortByValues(final Map<K, V> map) {
+	    Comparator<K> valueComparator =  new Comparator<K>() {
+	        public int compare(K k1, K k2) {
+	            int compare = map.get(k2).compareTo(map.get(k1));
+	            if (compare == 0) 
+	            	return 1;
+	            else 
+	            	return compare;
+	        }
+	    };
+	    Map<K, V> sortedByValues = new TreeMap<K, V>(valueComparator);
+	    sortedByValues.putAll(map);
+	    return sortedByValues;
 	}
+	
 	//displays the high scores, stops after 5 iterations
-	public static boolean display (TreeMap<String, Integer> hiscores, JFrame frame)
+	public static String display (TreeMap<String, Integer> hiscores)
 	{
 		Map<String, Integer> display = sortByValues(hiscores);
 		int i = 0;
-		String msg = "Name      |Score      \n";
+		String msg = "Name      |Score      <br/>";
 		for (Entry<String, Integer> entry : display.entrySet()) 
 		{
-			msg = entry.getKey() + ": " + entry.getValue();
+			msg += entry.getKey() + "      |" + entry.getValue() + "<br/>";
 			i++;
 			if (i == 5)
 				break;
 		}
-		JLabel label = new JLabel(msg);
-		frame.add(label);
-		
-		JButton playAgain = new JButton("Play Again");
-		playAgain.addActionListener(new ActionListener() {
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		    	frame.dispose();
-		    }
-		});
-		
-		JButton exitButton = new JButton("Exit");
-		exitButton.addActionListener(new ActionListener() {
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		    	frame.dispose();
-		    	System.exit(0);
-		    }
-		});
-		
-		return true;
-		
+		return msg;
 	}
-	/*	
-		 public static void main(String[] args) {
-			TreeMap<String, Integer> test = new TreeMap<String, Integer>();
-			test.put("Terry", 10); 
-			test.put("Larry", 7); 
-			test.put("Tom", 9); 
-			test.put("Mick", 8); 
-			test.put("Cale", 6); 
-		
-			
-			display(test); //Test initial display
-			System.out.println();
-			test = cool("Andy", 10, test);		
-			display(test);//Test Display with a new entry
-			System.out.println();
-			test = cool("Cale", 10, test);	
-			display(test);//Test Display with an updated entry
-			System.out.println();
-			test = cool("Andy", 3, test);		
-			display(test);//Test display with a failed updated entry
-		}
-		*/
 	
+	/*
+	public static void main(String[] args) {
+		TreeMap<String, Integer> test = new TreeMap<String, Integer>();
+		test.put("Terry", 10); 
+		test.put("Larry", 7); 
+		test.put("Tom", 9); 
+		test.put("Mick", 8); 
+		test.put("Cale", 6); 
+		
+		display(test); //Test initial display
+		System.out.println();
+		test = cool("Andy", 10, test);		
+		display(test);//Test Display with a new entry
+		System.out.println();
+		test = cool("Cale", 10, test);	
+		display(test);//Test Display with an updated entry
+		System.out.println();
+		test = cool("Andy", 3, test);		
+		display(test);//Test display with a failed updated entry
+	}*/
 }
--- MathGameProject/src/MathGame.java
+++ MathGameProject/src/MathGame.java
@@ -2,54 +2,87 @@ import java.util.*;
 import java.util.Map.Entry;
 import javax.swing.*;
 import java.awt.event.*;
-//TEST
+import java.util.Comparator;
+
 public class MathGame 
 {
-	Hiscore scorekeeper;
-	Music musicObj;
-	RandomGenerator questionMaker = new RandomGenerator();
-	Scanner sc = new Scanner(System.in);//scanner
-	static int interval;//time
-	//static Timer timer; //time
+	//Hiscore scorekeeper;
+	int max = 50;
+	int min = 1;
+	int number1, number2, answer, operation, score;
+	int counter = 0;
+	char operator = '?';
+	int choice = 0;
+	String name;
+	boolean decision = false;
+	//boolean playMusic = false;
+	
+	Random rand = new Random();
+	Hiscore hiscore = new Hiscore();
 	
 	//FOR START MENU
 	JFrame frame = new JFrame();//creating instance of JFrame  
 	JButton startButton = new JButton("START");//creating instance of JButton for submit
 	JButton exitButton = new JButton("Exit");//creating instance of JButton for exit
-	JLabel label1 = new JLabel("WELCOME TO OUR MATH GAME!"); //creating instance of Jlabel for prompt
+	JLabel label1 = new JLabel("WELCOME TO OUR MATH GAME!"); //creating instance of Jlabel for prompt	
+		
+	//TODO: LABEL ASKS WHAT IS YOUR NAME AND ACCEPTS IT INTO NAME 
+	JLabel label2 = new JLabel();
+	JLabel label3 = new JLabel("SCORE: " + score);
+	//JLabel label4 = new JLabel("Starting in...");
+	JTextField textbox = new JTextField(null, 20);
+	JButton enterButton = new JButton("Enter");
+	JButton mathEnterButton = new JButton("Enter");
 	
-	public TreeMap <String, Integer> makeLeaders()
+	public static TreeMap <String, Integer> makeLeaders()
 	{
 		TreeMap<String, Integer> defaultScores = new TreeMap<String, Integer>();
-		test.put("Terry", 10); 
-		test.put("Larry", 7); 
-		test.put("Tom", 9); 
-		test.put("Mick", 8); 
-		test.put("Cale", 6); 
+		defaultScores.put("Terry", 10); 
+		defaultScores.put("Larry", 7); 
+		defaultScores.put("Tom", 9); 
+		defaultScores.put("Mick", 8); 
+		defaultScores.put("Cale", 6); 
 		
 		return defaultScores;
 	}
 	
-	class startActionListener implements ActionListener //this action listener for the clear Button
+	TreeMap<String, Integer> leaderboard = new TreeMap<String, Integer>();
+	
+	
+	public void startGame()
 	{
-	      public void actionPerformed(ActionEvent e) 
-	      {
-	    	  TreeMap <String, Integer> leaderboard = new TreeMap<String, Integer>();
-	    	  leaderboard = makeLeaders();
-	  		
-	    	//  frame.remove(startButton);
-	    	//  frame.remove(exitButton);
-	    	//  frame.remove(label1);
-	    	  
-	    	  frame.removeAll();
-	    	  
-	    	  questionMaker.equation(1,50,frame,leaderboard);
-	    	  
-	    	  frame.add(startButton);//adding button in JFrame 
-	    	  frame.add(exitButton);//adding button in JFrame 
-	    	  frame.add(label1);//adding label in JFrame 
-	      }
-	}
+		leaderboard = makeLeaders();
+		
+		//possible start screen
+		frame.setSize(700,500);// 700 width and 500 height  
+		frame.setLayout(null);//using no layout managers  
+		frame.setVisible(true);//making the frame visible  
+		
+		//FIRST SCREEN COMPONENTS (start)
+		frame.add(startButton);//adding button in JFrame 
+		frame.add(exitButton);//adding button in JFrame 
+		frame.add(label1);//adding label in JFrame 
+		
+		//SECOND SCREEN COMPONENTS (name)
+		frame.add(label2);
+		frame.add(textbox);
+		frame.add(enterButton);
+		
+		//THIRD SCREEN COMPONENTS (math)
+		frame.add(mathEnterButton);
+		frame.add(label3); //add label2 for score
+		
+		//FOURTH SCREEN COMPONENTS (scores)
+		
+		startButton.setBounds(240,200,200,80);//x axis, y axis, width, height 
+		exitButton.setBounds(240,280,200,80);//x axis, y axis, width, height 
+		label1.setBounds(240,40,400,100);//x axis, y axis, width, height  
+		
+		startButton.addActionListener(new startActionListener());//adding action listener to start button
+		exitButton.addActionListener(new exitActionListener());//adding action listener to exit button
+		enterButton.addActionListener(new nameActionListener());//adding action listener for name
+		mathEnterButton.addActionListener(new mathActionListener());//adding action listener for name
+	}//end of startGame()
 	
 	class exitActionListener implements ActionListener //this action listener for the clear Button
 	{
@@ -59,50 +92,165 @@ public class MathGame
 	      }
 	}
 	
-	public void startGame()
+	class startActionListener implements ActionListener //this action listener for the clear Button
 	{
-		//possible start screen
-		frame.setSize(700,500);// 700 width and 500 height  
-		frame.setLayout(null);//using no layout managers  
-		frame.setVisible(true);//making the frame visible  
-		
-		frame.add(startButton);//adding button in JFrame 
-		frame.add(exitButton);//adding button in JFrame 
-		frame.add(label1);//adding label in JFrame 
-		
-		
-		startButton.setBounds(240,200,200,80);//x axis, y axis, width, height for clear button
-		exitButton.setBounds(240,280,200,80);//x axis, y axis, width, height for clear button
-		label1.setBounds(240,40,400,100);//x axis, y axis, width, height for clear button
-		
-		
-		startButton.addActionListener(new startActionListener());//adding action listener to clear button
-		exitButton.addActionListener(new exitActionListener());//adding action listener to submit button
-		
-	}//end of startGame()
-}
-/* FIX THIS
-	public void timing()
+	      public void actionPerformed(ActionEvent e) 
+	      {
+	    	  //clears the frame
+	    	  startButton.setBounds(0,0,0,0);
+	    	  exitButton.setBounds(0,0,0,0);
+	    	  label1.setBounds(0,0,0,0);
+	    	  //resets to new game if played again
+	    	  textbox.setText(null);
+	    	  label2.setText("What is your name: ");
+	    	  score = 0;
+	    	  //shows new components
+	    	  label2.setBounds(240,40,400,100);
+	    	  textbox.setBounds(225,200,500,50);
+	    	  enterButton.setBounds(225,250,100,100);
+	      }
+	}//end of startActionListen
+	
+	class nameActionListener implements ActionListener //this action listener for the clear Button
 	{
-	    int secs=10;
-	    int delay = 1000;
-	    int period = 1000;
-	    timer = new Timer();
-	    interval = (secs);
-	    System.out.println(secs);
-	    
-	    timer.scheduleAtFixedRate(new TimerTask() 
-	    {
-	        public void run() 
-	        {
-	            System.out.println(setInterval());
-	        }
-	    }, delay, period);
+	      public void actionPerformed(ActionEvent e) 
+	      {
+
+	    	  counter = 0;
+	    	  name = textbox.getText();
+	    	  label2.setBounds(0,0,0,0);
+	    	  enterButton.setBounds(0,0,0,0);
+	    	  mathEnterButton.setBounds(225,250,100,100);
+	    	  textbox.setText(null);
+		    
+		    		
+					label3.setBounds(500,0,100,100); //label for score
+					label2.setBounds(240,40,400,100);
+					
+					//CREATING QUESTION HERE
+					operation = rand.nextInt(3) + min;
+						
+					if (operation == 3)
+						max = 10; //if random generator picks a multiplication problem, makes max 10 to make it easier
+						
+					number1 = rand.nextInt(max) + min; //random number max and min
+					number2 = rand.nextInt(max) + min; 
+						
+					//if random generator picks a subtraction problem, ensures the final answer of the problem can't be negative by switching the numbers
+					if (operation == 2 && number1 < number2)
+					{
+						int tempNum = number1;
+						number1 = number2;
+						number2 = tempNum;
+					}
+				
+					answer = 0;
+					
+					if (operation == 1)
+					{
+						operator = '+';
+						answer = number1 + number2;
+					}
+					if (operation == 2)
+					{
+						operator = '-';
+						answer = number1 - number2;
+					}
+					if (operation == 3)
+					{
+						operator = '*';
+						answer = number1 * number2;
+					}
+					
+	
+					
+					//PRINTS THE QUESTION ON LABEL1
+					label2.setText("QUESTION:  " + number1 + " " + operator + " " + number2 + " = ?");
+						
+					//IF USER ANSWER IS RIGHT THEN ADD 1 TO SCORE
+					
+		 
+		    	
+	      }
 	}
-    
-	private static final int setInterval() 
+	
+	class mathActionListener implements ActionListener //this action listener for the clear Button
 	{
-	    if (interval == 1)
-	        timer.cancel();
-	    return --interval;
-*/
+	      public void actionPerformed(ActionEvent e) 
+	      {
+	    	  	counter += 1;
+	    	  	if(counter < 10)
+	    	  	{
+	    	  	//stores name value after textbox input
+	  			choice = Integer.parseInt(textbox.getText());
+	  			textbox.setText("");
+	  			
+	  			if (choice == answer)
+					score += 1;
+					//UPDATE THE SCORE LABEL
+				label3.setText("SCORE: " + score);
+				
+				textbox.setText(null);
+					
+				//CREATING QUESTION HERE
+				operation = rand.nextInt(3) + min;
+						
+				if (operation == 3)
+				max = 10; //if random generator picks a multiplication problem, makes max 10 to make it easier
+						
+				number1 = rand.nextInt(max) + min; //random number max and min
+				number2 = rand.nextInt(max) + min; 
+						
+				//if random generator picks a subtraction problem, ensures the final answer of the problem can't be negative by switching the numbers
+				if (operation == 2 && number1 < number2)
+				{
+					int tempNum = number1;
+					number1 = number2;
+					number2 = tempNum;
+				}
+				
+				answer = 0;
+					
+				if (operation == 1)
+				{
+					operator = '+';
+					answer = number1 + number2;
+				}
+				if (operation == 2)
+				{
+					operator = '-';
+					answer = number1 - number2;
+				}
+				if (operation == 3)
+				{
+					operator = '*';
+					answer = number1 * number2;
+				}
+					
+					//PRINTS THE QUESTION ON LABEL1
+					label2.setText("QUESTION:  " + number1 + " " + operator + " " + number2 + " = ?");
+		    	}
+	    	  	else
+	    	  	{
+	    	  		//HISCORE PART
+	    	  		label3.setBounds(0,0,0,0);
+	    	  		textbox.setBounds(0,0,0,0);
+	    	  		mathEnterButton.setBounds(0,0,0,0);
+	    	  		
+	    	  		//CALCULATE THE SCORES HERE 
+	    	  		leaderboard = hiscore.storeScore(name, score, leaderboard);
+	    	  		
+	    	  		//label2.setBounds(0,0,0,0); //CHANGE THE BOUNDS TO FIT WHOLE HIGH SCORES
+	    	  		label2.setText("<html>TOP 5 SCORES<br/>" + hiscore.display(leaderboard) + "<html>");
+	    	  		
+	    	  		
+	    	  		
+	    	  		startButton.setText("Play Again");
+	    	  		startButton.setBounds(200,200,200,80);
+	    			exitButton.setBounds(400,200,200,80);
+	    	  	}
+	    	  	
+	      }
+	    	  	
+	}
+}//end of class
