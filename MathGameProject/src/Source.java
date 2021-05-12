57253724d2069e7799206619e24bff94e9896426
ryankao0415 <61336628+ryankao0415@users.noreply.github.com>
2021-05-12 14:53:18 -0700

    Update Source.java

--- MathGameProject/src/Source.java
+++ MathGameProject/src/Source.java
@@ -1,5 +1,9 @@
+/*
+ * Description: The Source class is a driver class that creates an instance of the MathGame class and calls
+ * the startGame method to start the game.
+ */
 
-public class Source 
+public class Source
 {
 	public static void main(String[]args) 
 	{
